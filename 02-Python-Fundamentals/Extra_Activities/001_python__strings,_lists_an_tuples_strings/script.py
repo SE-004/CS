571000d332561a42321f20457aeedb27871eaed7
mariblan <99274158+mariblan@users.noreply.github.com>
2025-08-08 09:10:03 +0200

    Added extra activities for Strings, Lists and Tuples

---
+++ 02-Python-Fundamentals/Extra_Activities/001_python__strings,_lists_an_tuples_strings/script.py
@@ -0,0 +1,1 @@
+# Write your code for step 1 below
---
+++ 02-Python-Fundamentals/Extra_Activities/005_python__strings,_lists_an_tuples_lists__looping_through_lists/script.py
@@ -0,0 +1,6 @@
+students = ['Tom', 'Alice', 'Jorge', 'Sara', 'Paul', 'Sara'];
+fruits = ['Mango', 'Apple', 'Kiwi'];
+healthy_food = ['Brocoli', 'Kiwi', 'Salad'];
+
+# Write your code here
+
