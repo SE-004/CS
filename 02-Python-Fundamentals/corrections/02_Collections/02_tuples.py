6dfeee43d6731a3e7788c4212ea1d9afa034745c
mariblan <mariablancocanal@gmail.com>
2025-08-11 17:00:09 +0200

    Added corrections for tuples and lecture for dict and sets

--- 02-Python-Fundamentals/corrections/02_Collections/02_tuples.py
+++ 02-Python-Fundamentals/corrections/02_Collections/02_tuples.py
@@ -1,40 +1,62 @@
 # 1. Create a Tuple
 # Create a variable named my_tuple and assign it a tuple containing at least 5 items (they can be strings, numbers, or a mix).
-
+my_tuple = (1,2,3,4,5,2,3,2)
 # 2. Print the Tuple
 # Use the print() function to display the contents of my_tuple.
-
+print(my_tuple)
 # 3. Access Tuple Items
 # Print the first item of the tuple using an index of 0.
+print(my_tuple[0])
 # Print the last item of the tuple using a negative index.
+print(my_tuple[-1])
 
 # 4. Slice the Tuple
 # Print a slice of the tuple that includes some middle items
+print(my_tuple[1:4])
 
 # Print a slice that goes from the start of the tuple up to a certain index
+print(my_tuple[:3])
 
 # Print a slice that starts at a certain index and goes to the end
+print(my_tuple[2:])
 
 # 5.Check if an Item Exists
 # Use the in keyword to check if a certain item exists in my_tuple.
-
+print(2 in my_tuple)
+print(6 in my_tuple)
 # Print a message indicating whether the item is found.
+if 2 in my_tuple:
+    print("The number 2 is in the tuple")
+    
+if "banana" in my_tuple:
+    print("The word banana is in the tuple")
 
 # 6. Count and Index
 # Use the count() method to find how many times a specific value appears in my_tuple.
+print(my_tuple.count(2))
+print(my_tuple.count(3))
+print(my_tuple.count(1))
+print(my_tuple.count(10))
 
 # Use the index() method to find the position of the first occurrence of that value.
+print(my_tuple.index(2))
 
 # 7. Packing and Unpacking
 # Packing: You’ve already packed the tuple by assigning multiple values to my_tuple.
-
+print(len(my_tuple))
 # Unpacking: Unpack my_tuple into separate variables so that each variable corresponds to an item in the tuple 
+# num_1, num_2, num_3, num_4, num_5, num_6, num_7, num_8 = my_tuple;
 
 # Use the asterisk (*) in another unpacking scenario 
+num_1, num_2, *middle, num_7, num_8 = my_tuple;
+print(num_1, num_2)
 
 # 8. Joining Tuples
 # Create another tuple named another_tuple with a few items.
-
+another_tuple = (3,4,6,7,1,3)
 # Concatenate my_tuple and another_tuple using the + operator. Print the result.
-
+cocat_tuple = my_tuple + another_tuple;
+print(cocat_tuple)
 # Multiply one of your tuples by an integer and print the result to see the repeated items.
+multi_tuple = my_tuple * 3;
+print(multi_tuple)
--- 02-Python-Fundamentals/lectures/02-Python-Collections.py
+++ 02-Python-Fundamentals/lectures/02-Python-Collections.py
@@ -49,34 +49,84 @@ vegetables = ["carrot", "broccoli", "spinach"]
 
 # TUPLES
 # Tuples are immutable sequences, typically used to store collections of heterogeneous data.
-colors = ("red", "green", "blue")
-print(colors)
-print(colors[0])
+# colors = ("red", "green", "blue")
+# print(colors)
+# print(colors[0])
 
-more_colors = ("yellow", "purple", "orange")
+# more_colors = ("yellow", "purple", "orange")
 
-# Concatenating tuples
-all_colors = colors + more_colors
-print(all_colors)
-repeated_colors = colors * 2  # Repeating tuples
-print(repeated_colors)
+# # Concatenating tuples
+# all_colors = colors + more_colors
+# print(all_colors)
+# repeated_colors = colors * 2  # Repeating tuples
+# print(repeated_colors)
 
-print(colors.count("red"))  # Count occurrences of "red"
-print(colors.index("green"))  # Find the index of "green"
+# print(colors.count("red"))  # Count occurrences of "red"
+# print(colors.index("green"))  # Find the index of "green"
 
-# Unpacking tuples
-red, green, blue = colors
-print(red)   # Output: red
-print(green) # Output: green
-print(blue)  # Output: blue
+# # Unpacking tuples
+# red, green, blue = colors
+# print(red)   # Output: red
+# print(green) # Output: green
+# print(blue)  # Output: blue
 
-first, *rest = more_colors
-print(first)  # Output: yellow
-print(rest)   # Output: ['purple', 'orange']
+# first, *rest = more_colors
+# print(first)  # Output: yellow
+# print(rest)   # Output: ['purple', 'orange']
 
-start, second, *middle, previous, last = all_colors
-print(start)  # Output: red
-print(second) # Output: green
-print(middle) # Output: ['blue', 'yellow', 'purple']
-print(previous)  # Output: orange
-print(last)   # Output: orange
+# start, second, *middle, previous, last = all_colors
+# print(start)  # Output: red
+# print(second) # Output: green
+# print(middle) # Output: ['blue', 'yellow', 'purple']
+# print(previous)  # Output: orange
+# print(last)   # Output: orange
+
+# SETS
+# Sets are unordered collections of unique elements.
+fruits_set = set(fruits);
+print(fruits_set)
+
+fruits = list(fruits_set);
+print(fruits)
+
+numbers_1 = {1, 2, 3, 4}
+numbers_2 = {1, 2, 5, 6, 4}
+# print(numbers_1.difference(numbers_2))
+# print(numbers_1.symmetric_difference(numbers_2))
+
+print(numbers_1.intersection(numbers_2))
+num_union = numbers_1.union(numbers_2);
+print(num_union)
+print(numbers_1)
+
+numbers_1.update(numbers_2)
+print(numbers_1)
+
+for num in numbers_1:
+    if num == 3:
+        print(num)
+
+person = {
+    "name": "Alice",
+    "age": 36,
+    "address": {
+        "street": "Gartenstr.",
+        "city": "Berlin"
+    },
+    "hobbies": ["Singing", "Traveling"]
+}
+
+print(person)
+# print(person["address"]["city"])
+# person["address"]["city"] = "Madrid"
+# print(person["address"]["city"])
+# person["address"]["postalcode"] = 11111;
+# print(person["address"]["postalcode"])
+print(person["hobbies"][0])
+person["hobbies"][0] = "Reading";
+print(person["hobbies"])
+# print(person.get("name"))
+# print(person.keys())
+# print(person.values())
+
+
