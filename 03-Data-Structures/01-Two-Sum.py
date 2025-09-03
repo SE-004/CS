2a339d092f61b5c873d2601ecec4ec55a392c352
mariblan <mariablancocanal@gmail.com>
2025-09-03 15:43:33 +0200

    Added correction for Two Sum activity

---
+++ 03-Data-Structures/01-Two-Sum.py
@@ -0,0 +1,21 @@
+# You are given an array of integers, nums, and an integer, target. Your goal is to find indices of two distinct elements in nums whose sum equals target. 
+# You can’t use the same element twice, and you may return the indices in any order. You can assume there is exactly one valid solution for each input.
+count1 = 0;
+def two_sum(nums: list[int], target: int): 
+    n = len(nums)
+    for i in range(n):
+        for j in range(i+1, n):
+            count1+=1
+            if nums[j] == target - nums[i]:
+                return [i, j];
+
+print(two_sum([2,4,3,5,6], 5));
+
+count2=0;
+def two_sum_enumerate(nums: list[int], target: int): 
+    n = len(nums)
+    for i, num in enumerate(nums):
+        for j, item in enumerate(nums):
+            count2+=1
+            if item == target - num:
+                return [i, j];
