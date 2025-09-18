babb66760ad8c88976c2f32d255fc0dd033f5f02
mariblan <mariablancocanal@gmail.com>
2025-09-18 17:24:21 +0200

    Added live coding of book profile until step 4

--- 00-Live-Coding/006_python__collections_of_data_review_project__create_a_phone_book/script.py
+++ 00-Live-Coding/006_python__collections_of_data_review_project__create_a_phone_book/script.py
@@ -5,11 +5,14 @@
 # 1.  #### Create the variables: 
     
 #     *   `alphabet` - pass a string with all of the letters o the alphabet as its value.
-        
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
 #     *   `phone_book` - an empty dictionary.
-        
+phonebook = {}
+
 # 2.  #### Create an entry for each letter of the alphabet in the dictionary:
-    
+for letter in alphabet:
+    phonebook[letter.capitalize()] = {}
+
 #     *   The key should be named after the letter of the alphabet capitalize.
         
 #     *   The value should be an empty dictionary.
@@ -17,9 +20,10 @@
 # 3.  #### Print: 
     
 #     *   The `phone_book`.
-        
+# print(phonebook)
 #     *   All the keys in the `phone_book`.
-        
+# print(phonebook.keys())
+
 # 4.  #### Add inside the key `"A"`: 
     
 #     *   Three key-value pairs. 
@@ -27,29 +31,80 @@
 #     *   Each key should be the name of a person.
         
 #     *   Each value should be a phone number.
-        
+# phonebook["A"] = {"Albert":"1234", "Alberto":"3345", "Andreas":"9987"} 
+phonebook["A"].update({"Albert":"1234", "Alberto":"3345", "Andreas":"9987"})
+# phonebook["A"]["Albert"] = "1234"
+# phonebook["A"]["Alberto"] = "3345"
+# phonebook["A"]["Andreas"] = "9987"
+       
 # 5.  #### Print: 
     
 #     *   All the names inside the `"A"` dictionary.
-        
+print(phonebook["A"].keys())
+
 #     *   Print all the phones in the dictionary `"A"`.
-        
+print(phonebook["A"].values())        
 #     *   The `"A"` dictionary.
-        
+print(phonebook["A"])    
+     
 # 6.  #### Repeat the last two step for the dictionaries inside `"C"`, `"M"` and `"P"`.
-    
+phonebook['C']['Charlie'] = '020-233343'
+phonebook['C']['Ceaser'] = '010-23456'
+phonebook['C']['Case'] = '123-45678'    
+
+phonebook['M']['Max'] = '123-123-123'
+phonebook['M']['Maria'] = '231-123-123'
+phonebook['M']['Melvin'] = '231-123-121'
+
+phonebook["P"]["pino"] = "012-345"
+phonebook["P"]["pina"] = "0456-789"
+phonebook["P"]["pini"] = "0876-543"
+
+
 # 7.  #### Print all keys in the `phone_book`.
-    
+print(phonebook.keys())
+
 # 8.  #### Print all values in the `phone_book`.
-    
+print(phonebook.values())    
 
 # ### BONUS: Dictionary Lookup
 
 # 1.  Create the `name` variable and give it a value.
-    
+name = "Mara"   
 # 2.  Check if the `name` is in the corresponding entry in the `phone_book`.
+
+# for letter, people in phonebook.items():
+#     if name.lower() in people or name.capitalize() in people or name.upper() in people:
+#         print("Name found")
+#         break
+# else:
+#     print("Name not found")
+name_found = False;
+
+for letter in phonebook.keys():
+    for person in phonebook[letter].keys():
+        if name.lower() == person.lower():
+            name_found = True
+            print("Found name: ", person)
+            break
+if not name_found:
+    print("Name not found")
+
+
+
 # 3.  Create the `phone` variable and give it a value.
-    
+
 # 4.  Check if the `phone` is in any of the entries the `phone_book`.
     
 # 5.  Print the name of the person that the `phone` belongs to if it is in the `phone_book`.
+number_search = '231-123-111'   #Maria
+found = False
+
+for letter, people in phonebook.items():
+    for name, number in people.items():
+        if number_search in number:
+           print("Found number for: ", name)
+           found = True
+
+if not found:
+    print("Number not found")
--- 00-Live-Coding/007_python__collections_of_data_review_project__create_a_profile_book/script.py
+++ 00-Live-Coding/007_python__collections_of_data_review_project__create_a_profile_book/script.py
@@ -1,5 +1,172 @@
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-# write your code here
+# ==============================
+# Project: Create a Profile Book
+# ==============================
+
+# 1.  #### Create a variable for:
+    
+#     *   `person` - a list that stores the following items:
+person = [("first name", "John"),("last name", "Doe"), ("birthdate", "12-12-1986"),("age", "44"),("favorite color","blue")]
+#         *   A tuple that stores `"first name` and `"John"`.
+            
+#         *   A tuple that stores `"last name"` and `"Doe"`.
+#         *   A tuple that stores `"birthdate"` and `"12-12-1986"`.
+            
+#         *   A tuple that stores the string `"age"` and an integer number with the age of the person.
+#         *   A tuple that stores the `"favorite color"` and `"blue"`.
+            
+#     *   `people` - an empty list. 
+people = []
+#     *   `favorite_foods` - a tuple that stores:
+#         *   the string `"favorite foods"` 
+#         *   A list with the items `"hamburger"`, `"spaguetti carbonara"` and `"chicken alfredo"`.
+favorite_foods = ('favorite foods', ["hamburger", "spaguetti carbonara", "chicken alfredo"])
+            
+            
+#     *   `hobbies` - a tuple that stores:
+#         *   the string `"hobbies"`
+#         *    A list that with the items `"traveling"` and `"reading"`.
+hobbies = ('hobbies', ["traveling", "reading"])           
+            
+#     *   `movies` - a tuple that stores:
+#         *   the string `"favorite movies"`
+#         *   A list with the items `"Star Wars: Episode IV - A New Hope"` and `"Avengers: Endgame"`.
+movies = ("favorite movies", ["Star Wars: Episode IV - A new Hope", "Avengers: Endgame"])
+            
+#     *   `profile` - a dictionary with a key for each letter in the string `alphabet`. Each key should have an empty dictionary as its value.
+profile = {letter: {} for letter in alphabet}
+
+# 2.  #### Update the list inside the `favorite_foods` tuple: 
+#     *   Add 2 more items to the list.
+favorite_foods[-1].append("apples")
+favorite_foods[-1].append("bananas")
+print(favorite_foods)
+
+#     *   Remove the item that is your less favorite of all the foods.
+favorite_foods[1].remove("hamburger")
+#     *   Update the value of the last item to be your favorite food.
+favorite_foods[1][1]= "quesaddillas"   
+  
+# 3.  #### Update the list inside the `hobbies` tuple: 
+    
+#     *   Update the last hobby to be `"cooking"`.
+hobbies[1][-1] = "cooking"
+#     *   Add two more hobbies.
+hobbies[1].extend(["cycling","reading"])
+#     *   Update the value of your first hobby to be the same as third item.
+hobbies[1][0] = hobbies[1][2]
+#     *   Remove the last item.
+del hobbies[1][-1]
+# 4.  #### Update list inside the `movies`
+    
+#     *   Remove the first item.
+movies[1].pop(0)
+
+#     *   Add two new item at the end.
+movies[1].extend(["abc", "def"])     
+
+#     *   Update the last value to be `"The empire Strikes Back"`.
+movies[1][-1] = "The empire Strikes Back"
+print(movies)
+
+# 5.  #### Remove duplicates from our lists:
+    
+#     *   Transform into a list:
+        
+#         *   The tuple `favorite_foods`.
+            
+#         *   The tuple `hobbies`.
+            
+#     *   Update the nested lists of  `favorite_foods`  and `hobbies` by transforming them into sets to remove duplicated values.
+        
+#     *   Transform back into lists both nested sets in `favorite_foods` and `hobbies`.
+        
+#     *   Transform back the lists `favorite_foods` and `hobbies` into tuples.
+        
+# 6.  #### Store all the data in the `person` list:
+    
+#     *   Add the `favorite_foods` tuple.
+        
+#     *   Add the `hobbies` tuple.
+
+#     *   Add the `movies` tuple.
+        
+# 7.  #### Transform the `person` to be a dictionary.
+    
+# 8.  #### Store the person in the right entry of the profile:
+    
+#     *   Loop through the `profile`.
+#     *   Check if the key is the same as the first letter of the `person`'s last name.
+#     *   If it is, create a new key inside it following the format `"Doe, John"`.
+
+        
+# 9.  #### Print:
+    
+#     *   The `profile` key that has the same value as `"Doe, John"`.
+        
+#     *   All the keys from `profile`.
+        
+#     *   All the values from `profile`.
+        
+#     *   All the keys inside the`"Doe, John"`.
+        
+#     *   All the  values inside `"Doe, John"`.
+
+
+# 10.  #### Create some more profiles:
+    
+#     *   Update the `person` dictionary to have fresh new info in the keys:
+        
+#         *   `"first name"`
+            
+#         *   `"last name"`
+            
+#         *   `"birthdate"`
+            
+#         *   `"age"`
+            
+#         *   `"favorite color"`
+            
+#         *   `"favorite foods"`
+            
+#         *   `"hobbies"`
+            
+#         *   `"movies"`
+            
+#     *   Add the `person`'s new data to the `people` list (be careful not to create a reference when adding the data).
+        
+#     *   Repeat the last two steps for as many people you want.
+# 11.  #### Add the new profiles to the Profile Book:
+    
+#     *   Loop through both the `profile`'s keys and the `people` items ([nested loops](https://www.w3schools.com/python/gloss_python_for_nested.asp "Python Nested Loops")).
+        
+#     *   Check if the key in profile is the same as the `"last name"`'s first letter of the people item.
+        
+#     *   If it is, add a new key-value pair inside that dictionary key.
+        
+#         *   The key should be in the format of `"Doe, John"` by using the first and last name o the item.
+            
+#         *   The value should be the item if the `people`'s list.
+            
+# 12.  #### Print the final dictionary.
+    
+
+# ### Bonus
+
+# 1.  Print with a loop all the `profile`'s keys that have a value ([bool()](https://www.w3schools.com/python/ref_func_bool.asp "Python bool() Function")).
+# 2.  Print with a loop a message with an introduction of each one of the people added to the profile.
+    
+# 3.  Check if the hobby `"Hiking"` is included in any of the entries of the `profile` and print a message if it is.
+    
+# 4.  Print if there is a person named Laura in the `profile`.
+
+
+
+
+
+
+
+
 
 
 
